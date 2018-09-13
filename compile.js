2adc637968eeac594801546413d6e47b404018c9
Wadah Esam <wadah-wleed@hotmail.com>
2018-09-13 21:32:54 +0300

    finishing compile.js
    where you can compile your contracts

---
+++ compile.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+// generating the cross platform path ...
+// __dirname in this case means the project folder  
+const inboxPath = path.resolve(__dirname, 'contracts' , 'inbox.sol');
+
+// reading the contents of the file using the file system library 
+const source = fs.readFileSync(inboxPath , 'utf8');
+
+// compile the ( source ) file > that will output the bytecode and the ABI and 
+// many other stuff that we don't care about right now ,
+// and in order to make them available to the 
+// the rest of the code 
+// we will use the module.exports
+// in this case we made the moudle.exports exporting 
+// only the inbox contract cuz we don't have others
+module.exports = solc.compile(source,1).contracts[':Inbox'];
+
+
---
+++ test/inbox.test.js
@@ -0,0 +1,10 @@
+
+// useful for making asserstions < already in the node modules.
+const assert = require('asssert'); 
+
+// as long as you start using it , you will get what it does for us.
+const ganache = require('ganache-cli');
+
+// this starts with an upper case because it is a constructor that is used to make 
+// web3 instances  
+const Web3 = require('web3');
